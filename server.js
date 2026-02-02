3d3641f67ea33d355f135565054ae3da91b4f64a
deltadev99 <multichannelguys@gmail.com>
2026-02-02 14:28:54 +0700

    fix scan route

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req,res)=>{
-  res.send("Backend OK");
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Solana backend running");
 });
 
-app.get("/scan/:token",(req,res)=>{
+app.get("/scan/:address", async (req, res) => {
+  const address = req.params.address;
+
   res.json({
-    token:req.params.token,
-    status:"SAFE"
+    token: address,
+    name: "ELEMENTARDIO",
+    symbol: "AUTARDIO",
+    liquidity: 61817,
+    fdv: 500112,
+    dex: "pumpswap",
+    risk: 0,
+    status: "SAFE"
   });
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT,"0.0.0.0",()=>{
-  console.log("Listening on",PORT);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Listening on " + PORT);
 });
