cde26bba244d0cdf31c63cddcc78998534e413d8
deltadev99 <multichannelguys@gmail.com>
2026-02-02 14:21:39 +0700

    fix port

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
 const express = require("express");
-const cors = require("cors");
-
 const app = express();
-app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).send("Backend OK");
+app.get("/", (req,res)=>{
+  res.send("Backend OK");
 });
 
-app.get("/scan/:address", (req, res) => {
+app.get("/scan/:token",(req,res)=>{
   res.json({
-    token: req.params.address,
-    status: "SAFE"
+    token:req.params.token,
+    status:"SAFE"
   });
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Listening on", PORT);
+app.listen(PORT,"0.0.0.0",()=>{
+  console.log("Listening on",PORT);
 });
